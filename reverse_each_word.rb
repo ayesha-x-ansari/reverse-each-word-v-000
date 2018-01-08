800cf3c43dc0bbd46990dcc9685aaaf37e731ded
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-08 03:21:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,10 @@
 
-sentence = "Hi_again,_just_making_sure_it's_reversed!" 
+sentence = "Hi_again,_just_making_sure_it's_reversed!"
 def reverse_each_word(sentence)
   reversed = sentence.split("_").collect do |word|
   word.reverse
 end
-  puts reversed 
+  puts reversed
   reversed.join(" ")
 end
 reverse_each_word(sentence)
-
