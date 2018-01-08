60c84a91647ac12e1ccb11f1c71f1a0508772b47
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-08 03:16:29 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+
+sentence = "Hi again, just making sure it's reversed!" 
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  puts reversed 
+  reversed.join(" ")
+end
+reverse_each_word(sentence)
+
