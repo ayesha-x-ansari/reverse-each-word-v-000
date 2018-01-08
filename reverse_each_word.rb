a7670cac88f59a42544c3d04cb3130d45ef614fa
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-08 03:29:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   reversed = sentence.split(" ").collect do |word|
   word.reverse
 end
-  puts reversed
+#  puts reversed
 # reversed.join(" ")
 end
 reverse_each_word(sentence)
